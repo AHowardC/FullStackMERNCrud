51fc8dfef47a2bf65602debb833164049bf4eb5d
AHowardC <akil.howard1@gmail.com>
2019-07-14 23:09:42 -0400

    testing

---
+++ client/jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  setupFiles: ["<rootDir>/test/setup.js", "<rootDir>/test/shim.js"]
+};
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,9 +1,8 @@
 import React from "react";
-import "./Header.css";
 
 class Header extends React.Component {
   render() {
-    return <h2>Toolbox MERN Stack Application</h2>;
+    return <h1>Toolbox MERN Stack Application</h1>;
   }
 }
 
